0819221a4fa720c0fb83a9f1d8a650a7c43e79c8
LubovSoina <52804038+LubovSoina@users.noreply.github.com>
2020-02-06 00:52:26 +0300

    Update Образовательные запросы.sql

--- Образовательные запросы.sql
+++ Образовательные запросы.sql
@@ -15,7 +15,7 @@ select distinct team_users.context                                              
                 uet.toolTitle                                                 as 'Инструменты',
                 ui2.leaderID                                                  as 'Лидерайди за кого оставили запрос',
                 concat(ui2.lastname, ' ', ui2.firstname, ' ', ui2.middlename) as 'ФИО за кого оставили запрос'
-from (select distinct tu.userID,
+from (select distinct tu.userID,                                      #отбираем запросы людей, у которых есть команда
                       t.title  as team,
                       t.id     as teamID,
                       tu.role  as role,
@@ -26,7 +26,7 @@ from (select distinct tu.userID,
                left join people.context_team ct on tu.teamID = ct.teamID
                left join people.context c on ct.contextID = c.id
                left join labs.context lc on c.uuid = lc.uuid
-      where lc.id in (142, 146, 132, 130, 153, 135, 145, 139, 166, 140, 167, 144, 147, 131, 175)) team_users
+      where lc.id in (142, 146, 132, 130, 153, 135, 145, 139, 166, 140, 167, 144, 147, 131, 175)) team_users   #здесь можно вставить или удалить id контекстов (из LABS !!!)
          left join people.user_info ui on team_users.userID = ui.userID
          left join ple.user_info ui1 on ui1.leaderID = ui.leaderID
          inner join ple.user_edurequest ue on ui1.userID = ue.userID
@@ -34,6 +34,6 @@ from (select distinct tu.userID,
          left join ple.user_edurequest_tool uet on uet.userEdurequestID = ue.id
          left join ple.user_edurequest_user ueu on ue.id = ueu.userEdurequestID
          left join ple.user_info ui2 on ui2.userID = ueu.userID
-where ui.leaderID not in (285904, 120408, 56129, 253433, 79918, 66276, 12274, 310842, 326040, 292928, 62575, 301300,
+where ui.leaderID not in (285904, 120408, 56129, 253433, 79918, 66276, 12274, 310842, 326040, 292928, 62575, 301300,  #убираем лидерайди сотрудников
                           345006, 357686, 253657, 530684, 118193, 670928, 697574, 665799,
                           631044, 60731, 253433, 29709, 663034, 120408, 253657, 285904, 670928, 60731)
