07106e88aae6efceec70e4f33c53451d3d64fffd
LubovSoina <52804038+LubovSoina@users.noreply.github.com>
2020-05-08 11:58:21 +0300

    Update команды+контексты.sql

--- команды+контексты.sql
+++ команды+контексты.sql
@@ -15,21 +15,20 @@ select distinct c.id                                                        cont
                 p.boards as trello,
                 c1.title                                                    project_context,
                 p.createDT                                                  proj_createDT
-
-
 from (select * from people.team t where t.active = 1 and t.createDT > '2020-04-10 14:10:21') t
          left join people.university u on t.universityID = u.id
          left join people.team_user tu on tu.teamID = t.id
          left join people.context_team ct on t.id = ct.teamID
          left join people.context c on ct.contextID = c.id
-         left join people.project_team pt on t.id = pt.teamID
-         left join (select * from people.project p where p.active = 1) p on pt.projectID = p.id
-
+         left join (select pt.teamID, p.id, p.boards, p.guid, p.createDT,p.title
+                    from people.project_team pt
+                             inner join people.project p on pt.projectID = p.id
+                    where p.active = 1) p on t.id = p.teamID
          left join people.context_project cp on p.id = cp.projectID
          left join people.context c1 on c1.id = cp.contextID
          left join people.user_info ui on ui.userID = tu.userID
          left join people.user_tag ut on ut.userID = tu.userID
          left join people.tag tag on ut.tagID = tag.id
-where tag.guid like '%ss20%'
-group by c.id, c.title, t.title, t.id, ui.leaderID,
+where  tag.guid like '%ss20%'
+group by c.id, c.title, t.title, t.id, ui.leaderID, p.id,
          concat(ui.lastname, ' ', ui.firstname, ' ', ui.middlename), tu.role
